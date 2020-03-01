1d17835c8939ddddc032b59f99fac0c07c4902f8
Kamil <kljablon@gmail.com>
2020-03-01 20:15:39 +0100

    finished work on basic book details view

--- ebook_viewer/ebook_viewer/settings.py
+++ ebook_viewer/ebook_viewer/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 # LOGGING = {
 #     'version': 1,
 #     'disable_existing_loggers': False,
